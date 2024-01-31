c16662e1ef587dcec52f39ae985a7e37cc976687
IanYael1275 <ianyael1275@outlook.es>
2024-01-31 15:22:35 -0600

    second commit

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 
 const monstruos=async()=>{
     try{
-        const respuesta= await fetch('https://eldenring.fanapis.com/api/creatures?limit=100');
+        const respuesta= await fetch('https://eldenring.fanapis.com/api/creatures?limit=70');
         console.log(respuesta);
-        const datos=await respuesta.json();
+
+        if(respuesta.status===200){
+            const datos=await respuesta.json();
+        console.log(datos); 
+
+        } else if(respuesta.status===404){
+            console.log('Han desaparecido los servidores con magia, intenta de nuevo');
+        } else{
+            console.log('Entro a tierras desconocidas, intente de nuevo');
+        }
+        
     } catch(error){
         console.log(error);
     }
