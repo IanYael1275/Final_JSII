4d4591ae2e2738e11ce5cbf3aeee4ad57608c5f2
IanYael1275 <ianyael1275@outlook.es>
2024-01-31 19:28:59 -0600

    third commit

--- main.js
+++ main.js
@@ -1,12 +1,55 @@
 
-const monstruos=async()=>{
+function getCreatures(done){
+    const resultado=fetch('https://eldenring.fanapis.com/api/creatures');
+    resultado
+    .then(response=>response.json())
+    .then(data=>{
+        done(data)
+    });
+
+
+}
+
+/*getCreatures(data=>{ 
+    console.log(data);
+});*/
+
+
+getCreatures(data=>{
+    data.results.forEach(Creatures=>{
+        const article=document.createRange().createContextualFragment(`
+        <article>
+            <div class="imagen">
+            <img src="${Creatures.image}" alt="Criatura">
+            </div>
+            <h2>${Creatures.name}</h2>
+            <span>${Creatures.id}<span>
+        </article>
+        `);
+        const main=document.querySelector("main");
+        main.append(article);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+// En caso de ser valida la peticion muestra los datos en consola
+/*const cargarMonstruos=async()=>{
     try{
-        const respuesta= await fetch('https://eldenring.fanapis.com/api/creatures?limit=70');
+        const respuesta= await fetch('https://eldenring.fanapis.com/api/creatures?limit=80');
         console.log(respuesta);
 
         if(respuesta.status===200){
-            const datos=await respuesta.json();
-        console.log(datos); 
+            const info=await respuesta.json();
+            console.log(info); 
+            
 
         } else if(respuesta.status===404){
             console.log('Han desaparecido los servidores con magia, intenta de nuevo');
@@ -20,4 +63,4 @@ const monstruos=async()=>{
    
 }
 
-monstruos();
+cargarMonstruos();*/
